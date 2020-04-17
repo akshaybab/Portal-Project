a4e7f7b9a92addbf1b89e21a0ec4f95c93958bfe
sparklepup2468 <jcbrhee@hotmail.com>
2020-04-17 14:01:14 -0700

    horizontal portals and tetures

---
+++ Julia Unity Project confused/Assets/Scenes/Assets/Resources/DestroyProjectile.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class DestroyProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy (gameObject, 1f);
+    }
+
+}
---
+++ Julia Unity Project confused/Assets/Scenes/Assets/Transport.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController Controller; 
+    public Transform Player;
+    public Transform otherPortal;
+    public bool InPortal;  
+
+    public Vector3 correction; 
+    // Update is called once per frame
+    void Update()
+    {
+        if (InPortal) {
+            Vector3 portalToPlayer = Player.position - transform.position; 
+
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+            Debug.Log(dotProduct);
+            if (dotProduct < 0f) {
+                Controller.enabled = false; 
+                Player.position = otherPortal.position + correction;
+                Controller.enabled = true; 
+                InPortal = false; 
+                Debug.Log("Moved player");
+                Debug.Log(Player.position);
+
+            }
+        }
+    }   
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.tag == "Player") {
+            Debug.Log("Enter");
+            InPortal = true; 
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.tag == "Player") {
+            Debug.Log("Exit");
+            InPortal = false; 
+        }
+    }
+}
