8512b477be859d8117b414e471eea5e8b8535878
Julian <Julians20@lakesideschool.org>
2020-05-07 16:44:37 -0700

    Revert "trying to merge"
    This reverts commit b2457d48fd93c77d6dbbb879b0167b48f230ae6b.

---
+++ Assets/ShootProjectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootProjectile : MonoBehaviour
+{
+    public Rigidbody projectileObject;
+    public Transform barrelEnd;
+    
+    
+    void Update ()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody rocketInstance;
+            rocketInstance = Instantiate(projectileObject, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+            rocketInstance.AddForce(barrelEnd.forward * 1000);
+        }
+    }
+}
---
+++ playermovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController controller; 
+
+    public float speed = 15f; 
+
+    public float gravity = -9.8f; 
+
+    public float jumpHeight = 2f; 
+
+    Vector3 downVel; 
+
+    public Transform groundCheck; 
+
+    public float groundDistance = 0.5f; 
+
+    public LayerMask groundMask; 
+
+    bool isGrounded; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(isGrounded);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && downVel.y < 0 ) {
+            downVel.y = -2f; 
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z; 
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            downVel.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            Debug.Log("Jump");
+        }
+
+        downVel.y += gravity * Time.deltaTime; 
+
+        controller.Move(downVel * Time.deltaTime); 
+
+
+    }
+
+}
